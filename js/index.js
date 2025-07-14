47adc73ff423c5c45b82c417ffdd11e9ee3f3038
mkldsa <mkldsa@naver.com>
2025-07-14 11:39:42 +0900

    0714

--- js/index.js
+++ js/index.js
@@ -148,7 +148,7 @@ $(document).ready(function () {
 
         scR = true;
 
-        var targetScroll = 2500; // 절대 위치로 설정
+        var targetScroll = 2900; // 절대 위치로 설정
 
         $('html, body').animate({
             scrollTop: targetScroll
